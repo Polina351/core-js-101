dcc4089ddb5714b100a57460c878b287f50f070c
Polina Deniskina <polinaoioi67@gmail.com>
2023-10-29 23:50:28 +0300

    feat: create task3

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -65,8 +65,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const res = value.replace('!', '').split(' ');
+  return res[1] + res[2];
 }
 
 

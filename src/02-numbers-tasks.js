a3f2bae51ed025cfdfc22770d2a031b1f87bb2c9
Polina Deniskina <polinaoioi67@gmail.com>
2023-10-30 01:16:38 +0300

    feat: create task 13

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -36,7 +36,7 @@ function getRectangleArea(width, height) {
  *   0    => 0
  */
 function getCircleCircumference(radius) {
-  return 2 * 3.14 * radius;
+  return (2 * Math.PI * radius);
 }
 
 /**
@@ -51,8 +51,8 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return (value1 + value2) / 2;
 }
 
 /**
